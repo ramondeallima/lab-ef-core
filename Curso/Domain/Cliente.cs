f44ed383c396bae77a26ccb76d59f79c00497903
Ramon Almeida <ramon.lima@itixti.com.br>
2021-09-08 23:29:58 -0300

    Demonstração de configuração usando data annotations

--- Curso/Domain/Cliente.cs
+++ Curso/Domain/Cliente.cs
@@ -1,9 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace CursoEFCore.Domain
 {
+    [Table("Clientes")]
     public class Cliente
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
         public string Nome { get; set; }    
+
+        [Column("Contato")]
         public string Telefone { get; set; }
         public string CEP { get; set; }
         public string Estado { get; set; }
