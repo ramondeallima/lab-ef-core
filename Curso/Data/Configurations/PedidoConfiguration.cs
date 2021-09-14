c922ebe5bbe53500101130d2c23eb7712c85f0de
Ramon Almeida <ramon.lima@itixti.com.br>
2021-09-13 22:22:31 -0300

    Ajuste nos mapeamentos

--- Curso/Data/Configurations/PedidoConfiguration.cs
+++ Curso/Data/Configurations/PedidoConfiguration.cs
@@ -10,7 +10,7 @@ namespace Curso.Data.Configurations
         {
             builder.ToTable("Pedidos");
             builder.HasKey(p => p.Id);
-            builder.Property(p => p.IniciadoEm).HasDefaultValue().ValueGeneratedOnAdd();
+            builder.Property(p => p.IniciadoEm).HasDefaultValueSql("GETDATE()").ValueGeneratedOnAdd();
             builder.Property(p => p.StatusPedido).HasConversion<string>();
             builder.Property(p => p.TipoFrete).HasConversion<int>();
             builder.Property(p => p.Observacao).HasColumnType("VARCHAR(512)");
