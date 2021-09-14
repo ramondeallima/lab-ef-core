b197d66e90a3f04b03b3c5504d27a2cb6df25951
Ramon Almeida <ramondeallima@gmail.com>
2021-09-13 23:42:25 -0300

    Update do banco na execução do projeto

--- Curso/Program.cs
+++ Curso/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Curso.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace CursoEFCore
 {
@@ -6,6 +8,13 @@ namespace CursoEFCore
     {
         static void Main(string[] args)
         {
+
+            using var db = new ApplicationContext();
+
+            // O Migrate é utilizado para atualizar o banco sempre que a aplicação for executada.
+            // OBS: Esse procedimento é recomendado somente para para ambiente local/dev.
+            db.Database.Migrate();
+
             Console.WriteLine("Hello World!");
         }
     }
