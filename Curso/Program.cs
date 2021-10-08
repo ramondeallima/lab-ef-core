584d03ca9f5794fa9bf73dcf0f53b5edda22dbe0
Ramon Almeida <ramondeallima@gmail.com>
2021-10-08 02:36:52 -0300

    Verificação de migrações pendentes

--- Curso/Program.cs
+++ Curso/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Curso.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,7 +14,18 @@ namespace CursoEFCore
 
             // O Migrate é utilizado para atualizar o banco sempre que a aplicação for executada.
             // OBS: Esse procedimento é recomendado somente para para ambiente local/dev.
-            db.Database.Migrate();
+            // db.Database.Migrate();
+
+
+            // Maneira para verificar se existem migrações pendentes.
+            // Esse recurso pode ser útil para tratativas em ambientes local.
+
+            var existsUpdates = db.Database.GetPendingMigrations().Any();
+
+            if (existsUpdates)
+            {
+                
+            }
 
             Console.WriteLine("Hello World!");
         }
